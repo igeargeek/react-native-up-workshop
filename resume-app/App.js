5cb16ab3dc092065b220491116e98d9c12859717
chonlatit inkaew <>
2020-09-16 15:25:55 +0700

    [Added] add resume list page

--- resume-app/App.js
+++ resume-app/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import ResumeForm from './src/screens/ResumeForm'
 import ResumeDetail from './src/screens/ResumeDetail'
+import ResumeList from './src/screens/ResumeList'
 
 const Stack = createStackNavigator();
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="ResumeList" options={{ title: 'Resume list' }} component={ResumeList} />
         <Stack.Screen name="ResumeForm" options={{ title: 'Resume form' }} component={ResumeForm} />
         <Stack.Screen name="ResumeDetail" options={{ title: 'Resume Detail' }} component={ResumeDetail} />
       </Stack.Navigator>
