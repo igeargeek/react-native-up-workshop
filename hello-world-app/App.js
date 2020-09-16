96571ea92faf8128d1d90c2250e3010325a47051
chonlatit inkaew <>
2020-09-16 12:07:57 +0700

    [Edited] hello word page

--- hello-world-app/App.js
+++ hello-world-app/App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [input, setInput] = useState('')
+  const [textColor, setTextColor] = useState('black')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hello World</Text>
+      <TextInput
+        value={input} onChangeText={(text) => setInput(text)}
+        placeholder="Input here"
+        style={styles.input}
+      />
+      <Text style={{ color: textColor, fontSize: 18 }}>{input}</Text>
+      <View style={styles.buttonLayout} >
+        <Button title="Red" color="red" onPress={() => setTextColor('red')} />
+        <Button title="Blue" color="blue" onPress={() => setTextColor('blue')} />
+        <Button title="Green" color="green" onPress={() => setTextColor('green')} />
+      </View>
     </View>
   );
 }
@@ -17,5 +29,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 30
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    width: '100%',
+    marginBottom: 20
+  },
+  buttonLayout: { 
+    flexDirection: 'row',
+    marginTop: 20
+  }
 });
